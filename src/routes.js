003f450dd785a62a884ac4786e0c0c31dcb64fb0
Felipe <felipe.seraf@gmail.comgit config --global user.name Felipegit coomit -m Firstgit config --global user.email felipe.seraf@gmail.com>
2019-10-18 10:25:14 -0300

    Consertei a cagada

--- src/routes.js
+++ src/routes.js
@@ -17,7 +17,7 @@ routes.get('/users/:id', UserController.getById);
 routes.post('/posts', upload.single('image'), PostController.store);
 routes.post('/users', upload.single('image'), UserController.store);
 
-routes.post('/users/:id/point', RankingController.update);
+routes.put('/users/:id/point', upload.single('image'), RankingController.update);
 
 routes.post('/posts/:id/like', LikeController.store);
 routes.post('/users/:id/point', RankingController.store);
